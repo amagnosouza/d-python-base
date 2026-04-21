7979debca1b5a3f68767ad375609aedcccc0f168
Alexandre Souza <alexandremagnofs@gmail.com>
2026-04-21 14:49:29 -0300

    feat: add shebang and comment

--- day01/hello.py
+++ day01/hello.py
@@ -1,1 +1,5 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+
+# A simple "Hello, World!" program in Python. | This is a comment.
+
+print("Hello, World!") # This line prints the string "Hello, World!" to the console.
