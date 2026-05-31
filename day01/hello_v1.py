6658cbfa3042725f3d87a12de556842d3c1b69e2
Alexandre Souza <alexandremagnofs@gmail.com>
2026-05-31 19:48:22 -0300

    feat: add multilanguage Hello World script with environment language support

---
+++ day01/hello_v1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Hello World multilanguage program in Python.
+
+Depending on the language configured in the environment, the output
+message will change accordingly.
+
+How to run:
+Set the environment variable `LANG` to the desired language
+(for example `en_US.UTF-8` for English, `fr_FR.UTF-8` for French)
+and run this script.
+
+Example:
+    export LANG=en_US.UTF-8
+    python3 hello.py
+
+Output:
+    Hello World!
+"""
+# Metadata about the script
+__version__ = "0.1.1"
+__author__  = "Alexandre Souza"
+
+import os
+
+current_lang = os.getenv("LANG", "pt_BR")[:5]  # Default language (first 5 chars)
+
+# Messages mapping for supported languages
+messages = {
+    "pt_BR": "Olá, Mundo!",
+    "fr_FR": "Bonjour, le Monde!",
+    "es_ES": "¡Hola, Mundo!",
+    "de_DE": "Hallo, Welt!",
+    "en_US": "Hello, World!",
+}
+
+# Select message based on current language, defaulting to English
+msg = messages.get(current_lang, "Hello, World!")
+
+print(msg)
