8c8c998b1e2a0f29bb7c0d7dcd31182f3a61a850
Alexandre Souza <alexandremagnofs@gmail.com>
2026-06-26 22:36:31 -0300

    feat: enhance logging with detailed error messages in logs.py

--- day03/logs.py
+++ day03/logs.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3 
 
+import logging # docs.python.org/3/library/logging.html#logging-levels
+
+# instanciando o logger
+log = logging.Logger("logs.py", logging.DEBUG)
+
+# levels: DEBUG, INFO, WARNING, ERROR, CRITICAL
+
+# formatacao: logging.<level>(<mensagem>)
+
+# destino: console, arquivo, etc
+
+log.debug("Mensagem de debug para desenvolvedores")
+log.info("Mensagem de informação para usuários")
+log.warning("Mensagem de aviso que algo inesperado aconteceu")
+log.error("Mensagem de erro que afetou o funcionamento do programa")
+log.critical("Mensagem de erro crítico que afetou o funcionamento do programa e precisa de atenção imediata")
+
+print("---")
+
 try:
     1 / 0
 except ZeroDivisionError as e:
-    print(f"Deu Ruim: {e}")
+    log.error("Deu Ruim! %s", str(e))
